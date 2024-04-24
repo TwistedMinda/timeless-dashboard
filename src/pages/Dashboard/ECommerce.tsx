9469bc5cbcbde7bab2b5e44e224e6fd0dbd672a2
Julien Brunet <julien.brunet.92@gmail.com>
2024-04-24 19:50:35 +0200

    first commit

---
+++ src/pages/Dashboard/ECommerce.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import CardDataStats from '../../components/CardDataStats';
+import ChartOne from '../../components/Charts/ChartOne';
+import ChartThree from '../../components/Charts/ChartThree';
+import ChartTwo from '../../components/Charts/ChartTwo';
+import ChatCard from '../../components/Chat/ChatCard';
+import MapOne from '../../components/Maps/MapOne';
+import TableOne from '../../components/Tables/TableOne';
+import DefaultLayout from '../../layout/DefaultLayout';
+import ProductOne from '../../images/product/product-01.png';
+import ProductTwo from '../../images/product/product-02.png';
+import ProductThree from '../../images/product/product-03.png';
+import ProductFour from '../../images/product/product-04.png';
+
+interface Ambiance {
+  name: string
+  image: string
+  category: string
+}
+
+const Ambiances = () => {
+  const productData: Ambiance[] = [
+    {
+      image: ProductOne,
+      name: 'Apple Watch Series 7',
+      category: 'Electronics',
+    },
+    {
+      image: ProductTwo,
+      name: 'Macbook Pro M1',
+      category: 'Electronics',
+    },
+    {
+      image: ProductThree,
+      name: 'Dell Inspiron 15',
+      category: 'Electronics',
+    },
+    {
+      image: ProductFour,
+      name: 'HP Probook 450',
+      category: 'Electronics',
+    },
+  ]
+  return (
+    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="py-6 px-4 md:px-6 xl:px-7.5">
+        <h4 className="text-xl font-semibold text-black dark:text-white">
+          Top Products
+        </h4>
+      </div>
+
+      <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
+        <div className="col-span-3 flex items-center">
+          <p className="font-medium">Product Name</p>
+        </div>
+        <div className="col-span-2 hidden items-center sm:flex">
+          <p className="font-medium">Category</p>
+        </div>
+      </div>
+
+      {productData.map((product, key) => (
+        <div
+          className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
+          key={key}
+        >
+          <div className="col-span-3 flex items-center">
+            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+              <div className="h-12.5 w-15 rounded-md">
+                <img src={product.image} alt="Product" />
+              </div>
+              <p className="text-sm text-black dark:text-white">
+                {product.name}
+              </p>
+            </div>
+          </div>
+          <div className="col-span-2 hidden items-center sm:flex">
+            <p className="text-sm text-black dark:text-white">
+              {product.category}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const ECommerce: React.FC = () => {
+  return (
+    <DefaultLayout>
+      <Ambiances />
+    </DefaultLayout>
+  );
+};
+
+export default ECommerce;
